2563d197f23531f973202ed551216429a8a48739
Csávás Gergely <csavas.g95@gmail.com>
2022-03-25 00:36:00 +0100

    - Angular Material hozzáadása a projekthez (kell a formhoz) - Oldal kinézetének optimalizálása - Animációk hozzáadása a működéshez - Ajándék SVG kép hozzáadása a projekthez

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, transition, animate, style } from "@angular/animations";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({transform: 'translateY(-100%)'}),
+        animate('1000ms ease-in', style({transform: 'translateY(0%)'}))
+      ]),
+      transition(':leave', [
+        animate('1000ms ease-in', style({transform: 'translateY(-100%)'}))
+      ])
+    ]),
+    trigger('slideInOutReversed', [
+      transition(':enter', [
+        style({transform: 'translateY(300%)'}),
+        animate('1000ms ease-in', style({transform: 'translateY(0%)'}))
+      ]),
+      transition(':leave', [
+        animate('1000ms ease-in', style({transform: 'translateY(300%)'}))
+      ])
+    ])
+  ]
 })
-export class AppComponent {
-  title = 'Evfordulo';
+export class AppComponent implements OnInit {
+  isFormLinear = false;
+  firstGroup = FormGroup;
+  secondGroup = FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  title = 'Üdvözlünk titeket!';
+  giftVisible = true;
+  formVisible = false;
+
+  ngOnInit() {
+    // this.firstGroup = this.formBuilder.group({
+    //   firstCtrl: ['', Validators.required],
+    // });
+    // this.secondGroup = this.formBuilder.group({
+    //   secondCtrl: ['', Validators.required],
+    // })
+  }
+
+  onGiftClick() {
+    this.giftVisible = false;
+
+    setTimeout(() => {this.formVisible = true}, 1001)
+
+    this.title = "Nemsokára kinyílik..."
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatStepperModule} from "@angular/material/stepper";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatNativeDateModule} from "@angular/material/core";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
